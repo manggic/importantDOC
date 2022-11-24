e13dc40688c8538f2c9ff6de849f94c055304a34
Manish <manish.mahto@techno-soft.in>
2022-11-24 11:18:00 +0530

    added new regex

--- regex.js
+++ regex.js
@@ -1,6 +1,4 @@
 
-
-
 // + : match 1 or more
 // * : match 0 or more
 // \w : match all word
@@ -9,18 +7,22 @@
 // [a-zA-Z0-9] : match char from a to z OR A to Z or 0 to 9
 
 
-
-const str = 'If the g flag is used, all results matching the complete regular expression will be returned.'
-
+const str = 'If the g flag is used, all results $#%^&*$@#matching the complete regular expression will be returned.'
 
 
 // remove space from string
 let removeSpace = str.replace(/\s/g, '')
 
-
 // remove all spaces and comma
 let removeSpaceAndComma = str.replace(/[, ]/g,'')
 
+// remove all special char
+let removeSpecialChar = str.replace(/[^\w\s]/gi,'')
 
-console.log(removeSpaceAndComma);
+// match word starting with 're'
+let matchWordStartingWith = str.match(/\bre\w+/gi,'')
+
+
+
+console.log(matchWordStartingWith);
 
